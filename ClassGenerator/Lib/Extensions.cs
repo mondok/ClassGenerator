da182185aa529053da5994fd557060d9410c80c1
Matt Mondok <matt.mondok@gmail.com>
2015-03-17 10:30:54 -0400

    Proper naming of csharp properties

---
+++ ClassGenerator/Lib/Extensions.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassGenerator.Lib
+{
+    /// <summary>
+    /// Useful extension methods
+    /// </summary>
+    public static class Extensions
+    {
+        /// <summary>
+        /// Creates a properly named CSharp property
+        /// </summary>
+        /// <param name="value">The value.</param>
+        /// <returns>System.String.</returns>
+        public static string CsharpifyProperty(this string value)
+        {
+            if (string.IsNullOrEmpty(value)) return String.Empty;
+            string output = String.Empty;
+            string okProps = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
+            for(int i = 0; i < value.Length; i++)
+            {
+                bool upperNext = false;
+                char c = value[i];
+                if (i == 0)
+                {
+                    output = c.ToString().ToUpper();
+                }
+                else
+                {
+                    if (okProps.Any(s => s == c))
+                    {
+                        output += upperNext ? c.ToString().ToUpper() : c.ToString();
+                        upperNext = false;
+                    }
+                    else
+                    {
+                        upperNext = true;
+                    }
+                }
+            }
+            return output;
+        }
+    }
+}
